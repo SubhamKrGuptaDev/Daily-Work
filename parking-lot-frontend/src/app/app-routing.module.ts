d30226df3c2c81ad36f5d5386f8385a210e1efd8
sgupta1 <subham.kumar.gupta@adp.com>
2025-01-06 17:40:00 +0530

    add all the files with routers

--- parking-lot-frontend/src/app/app-routing.module.ts
+++ parking-lot-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { VehicleComponent } from './components/vehicle/vehicle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: 'vehicle', component: VehicleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ parking-lot-frontend/src/app/components/vehicle/vehicle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'vehicle-app',
+    templateUrl: './vehicle.component.html',
+    styleUrls: ['./vehicle.component.css']
+})
+export class VehicleComponent {
+    title = 'parking-lot-frontend';
+}
