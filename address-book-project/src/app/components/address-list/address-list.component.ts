fd2f6d1e9f34b4a17be9eada9ec4df26df951d76
Subham <subham7031979@gmail.com>
2025-01-09 17:59:45 +0530

    address details changes

--- address-book-project/src/app/components/address-list/address-list.component.ts
+++ address-book-project/src/app/components/address-list/address-list.component.ts
@@ -13,4 +13,20 @@ export class AddressListComponent {
   constructor(private share: SharedService) {
     this.addressArray = share.addressObject;
   }
+
+  viewAddress(address: Address) {
+    this.setAddress(address);
+  }
+
+  setAddress(address: Address) {
+    this.share.setCurAddress(address);
+    // this.share.curSelect.name = address.name;
+    // this.share.curSelect.email = address.email;
+    // this.share.curSelect.mobileNo = address.mobileNo;
+    // this.share.curSelect.city = address.city;
+    // this.share.curSelect.country = address.country;
+    // this.share.curSelect.description = address.description;
+    // this.share.curSelect.pinCode = address.pinCode;
+    // this.share.curSelect.state = address.state;
+  }
 }
