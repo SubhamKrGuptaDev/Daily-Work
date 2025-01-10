b0355dcad96908b449c4fd5fc788b99c2ad0b3dd
Subham <subham7031979@gmail.com>
2025-01-10 18:03:23 +0530

    controller and service added

---
+++ address-book/src/main/java/com/address/book/constants/CommonConstants.java
@@ -0,0 +1,7 @@
+package com.address.book.constants;
+
+public class CommonConstants {
+
+    public final static String DELETE_MESSAGE_SUCCESSFUL = "Delete successful";
+
+}
--- address-book/src/main/java/com/address/book/controller/AddressController.java
+++ address-book/src/main/java/com/address/book/controller/AddressController.java
@@ -3,11 +3,11 @@ package com.address.book.controller;
 import com.address.book.entity.Address;
 import com.address.book.service.AddressService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static com.address.book.constants.CommonConstants.DELETE_MESSAGE_SUCCESSFUL;
 
 @RestController
 @RequestMapping("/address")
@@ -22,6 +22,27 @@ public class AddressController {
     @GetMapping
     public ResponseEntity<List<Address>> getAll() {
         return ResponseEntity.ok(addressService.findAll());
+    }
+
+    @GetMapping(params = "id")
+    public ResponseEntity<Address> getById(@RequestParam Integer id) {
+        return ResponseEntity.ok(addressService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Address> create(@RequestBody Address address) {
+        return ResponseEntity.ok(addressService.save(address));
+    }
+
+    @PutMapping
+    public ResponseEntity<Address> update(@RequestBody Address address) {
+        return ResponseEntity.ok(addressService.update(address));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> delete(@RequestParam Integer id) {
+        addressService.delete(id);
+        return ResponseEntity.ok(DELETE_MESSAGE_SUCCESSFUL);
     }
 
 }
