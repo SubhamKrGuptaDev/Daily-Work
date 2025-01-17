ccadb0a079723722ec3a85c1b1fb1cea4bd29dbc
Subham <subham7031979@gmail.com>
2025-01-17 10:52:43 +0530

    add components and bootstrap

--- angular practice/example-event-emitter/src/app/app.module.ts
+++ angular practice/example-event-emitter/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserViewComponent } from './components/user-view/user-view.component';
+import { UserItemComponent } from './components/user-item/user-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserListComponent,
+    UserViewComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule,
